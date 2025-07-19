c80c205353143c95e53a6f390e0ddd1caba7356c
Priti Gupta <civilstudent9900@gmail.com>
2025-07-19 14:48:59 +0530

    fix: correct text formatting in Home component header

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="max-w-7xl  p-8 flex flex-col md:flex-row items-center justify-between mt-10 mx-auto md:gap-4">
      <div className="flex flex-col items-start justify-between gap-4 md:w-1/2">
    
-      <h1 className="font-bold text-2xl md:text-5xl w-full">Home-Style Meals, Just a Click Away</h1>
+      <h1 className="font-bold text-2xl md:text-5xl w-full">Home-Style Meals, Just a <span className="text-orange-600">Click</span> Away</h1>
       <p className="text-sm text-gray-400">Tiffin Box brings fresh, affordable meals from nearby restaurants to busy professionals. No cooking, no stress—just delicious food delivered to your office.</p>
      
       <div className="relative w-full">
